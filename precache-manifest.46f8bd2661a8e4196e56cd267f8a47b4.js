60d2e9f992e8b62e4c8e933b41aa0ad6b7cd31ba
Marcel Barros Aparici <barros.marcel92@gmail.com>
2018-10-17 19:21:33 +0200

    Updates

---
+++ precache-manifest.46f8bd2661a8e4196e56cd267f8a47b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8434788a761e46214e28",
+    "url": "/static/js/main.8434788a.chunk.js"
+  },
+  {
+    "revision": "2177a90793c0f9a8fa01",
+    "url": "/static/js/1.2177a907.chunk.js"
+  },
+  {
+    "revision": "86b16269cd3340371039866f4e9a3bc4",
+    "url": "/index.html"
+  }
+];
