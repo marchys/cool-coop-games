70f8f867aecdfc8a31eee9a5a403badd037e9171
Unknown <alberto.monte.gar@gmail.com>
2019-01-23 22:18:15 +0100

    Delete component that is not being used

--- src/gameListContainer/components/gameListContainer.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import gameList from './gameListContainer';
-
-it('should render', () => {
-  const wrapper = shallow(<gameList />);
-  expect(wrapper.getElement).not.toBe(null);
-});
