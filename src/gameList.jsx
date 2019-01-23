ff33ce3127423bcaea2035d769363efd8905d9d2
Unknown <alberto.monte.gar@gmail.com>
2019-01-23 22:17:39 +0100

    Add real list of games

--- src/gameList.jsx
+++ src/gameList.jsx
@@ -2,24 +2,27 @@ import { VisibilityFilters } from './filterSelector/redux/actions';
 
 const gameList = {
   '0': {
-    title: 'Name 1',
+    title: 'Wizard of Legend',
     status: VisibilityFilters.SHOW_PENDING,
+    steamId: '445980',
   },
   '1': {
-    title: 'Name 2',
+    title: 'Destiny 2',
     status: VisibilityFilters.SHOW_PLAYING,
   },
   '2': {
-    title: 'Name 3',
-    status: VisibilityFilters.SHOW_FINISHED,
-  },
-  '3': {
-    title: 'Name 4',
+    title: 'The Division',
     status: VisibilityFilters.SHOW_PENDING,
   },
+  '3': {
+    title: 'Divinity 2',
+    status: VisibilityFilters.SHOW_PENDING,
+    steamId: '435150',
+  },
   '4': {
-    title: 'Name 5',
-    status: VisibilityFilters.SHOW_FINISHED,
+    title: 'Binding of Isaac',
+    status: VisibilityFilters.SHOW_PENDING,
+    steamId: '2405',
   },
 };
 
